4648ac4a40c22b02edeb8b3fbb0a8f567f3cfada
Eduard Fakhrutdinov <edikf03@gmail.com>
2026-04-27 16:22:48 +0300

    fix: typescript easing types

--- src/pages/Home/IntroSection/IntroSection.tsx
+++ src/pages/Home/IntroSection/IntroSection.tsx
@@ -26,7 +26,7 @@ const IntroSection: React.FC<IntroSectionProps> = ({ id }) => {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.6, ease: "easeOut" },
+      transition: { duration: 0.6, ease: "easeOut" as const },
     },
   };
 
